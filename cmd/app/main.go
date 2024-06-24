126821662a20e520dd2ebab90daf943198c55b97
duardoqueiroz <eduardoldq1@gmail.com>
2024-06-24 14:54:45 -0300

    feat: inital setup

---
+++ cmd/app/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/duardoqueiroz/my-finances-golang/pkg/config"
+)
+
+func main() {
+	config.ReadConfig()
+}
---
+++ pkg/config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/spf13/viper"
+)
+
+type config struct {
+	Database struct {
+		User     string
+		Password string
+		Port     string
+		Name     string
+		Host     string
+	}
+	Server struct {
+		Port string
+	}
+}
+
+var C config
+
+func ReadConfig() {
+	config := &C
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(path.Join(rootDir(), "config"))
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		log.Fatalln(err)
+	}
+
+	if err := viper.Unmarshal(config); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	spew.Dump(C)
+}
+
+func rootDir() string {
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	return filepath.Dir(d)
+}
