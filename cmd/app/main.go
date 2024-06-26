dba65268c270b3ae327626ebfaec2e05c7e9b73e
duardoqueiroz <eduardoldq1@gmail.com>
2024-06-26 12:22:43 +0000

    feat: open server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/duardoqueiroz/my-finances-golang/pkg/config"
 )
 
 func main() {
 	config.ReadConfig()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	http.ListenAndServe(fmt.Sprintf(":%s", config.C.Server.Port), nil)
 }
