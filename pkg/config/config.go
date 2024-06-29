7ad3009e53af55b84b8a6442af51cec1159c1943
duardoqueiroz <eduardoldq1@gmail.com>
2024-06-29 19:47:14 +0000

    feat: change configuration strategy

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,60 +2,60 @@ package config
 
 import (
 	"fmt"
-	"path"
-	"path/filepath"
-	"runtime"
+	"os"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/go-playground/validator/v10"
-	"github.com/spf13/viper"
+	"github.com/duardoqueiroz/my-finances-golang/pkg/infra/api"
+	"github.com/duardoqueiroz/my-finances-golang/pkg/infra/database"
 )
 
 type config struct {
-	Database struct {
-		Type     string `mapstructure:"type" validate:"required"`
-		User     string `mapstructure:"user" validate:"required"`
-		Password string `mapstructure:"password" validate:"required"`
-		Port     string `mapstructure:"port" validate:"required"`
-		Name     string `mapstructure:"name" validate:"required"`
-		Host     string `mapstructure:"host" validate:"required"`
-	} `mapstructure:"database"`
-	Server struct {
-		Port string `mapstructure:"port" validate:"required"`
-		Host string `mapstructure:"host" validate:"required"`
-	} `mapstructure:"server"`
+	appname           string
+	db                database.Connection
+	repositoryHandler database.RepositoryHandler
+	server            api.Server
 }
 
-func ReadConfig() (*config, error) {
-	config := &config{}
-	viper.SetConfigName("config")
-	viper.SetConfigType("json")
-	viper.AddConfigPath(path.Join(rootDir(), "config"))
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err != nil {
-		return nil, fmt.Errorf("error reading config file, %s", err)
-	}
-
-	if err := viper.Unmarshal(config); err != nil {
-		return nil, fmt.Errorf("unable to decode into struct, %v", err)
-	}
-
-	validate := validator.New()
-	if err := validate.Struct(config); err != nil {
-		validationErrors := err.(validator.ValidationErrors)
-		for _, e := range validationErrors {
-			fmt.Println(e)
-		}
-		return nil, fmt.Errorf("error validating config, %v", err)
-	}
-
-	spew.Dump(config)
-	return config, nil
+func New() *config {
+	return &config{}
 }
 
-func rootDir() string {
-	_, b, _, _ := runtime.Caller(0)
-	d := path.Join(path.Dir(b))
-	return filepath.Dir(d)
+func (c *config) AppName(appname string) *config {
+	c.appname = appname
+	return c
+}
+
+func (c *config) Database(instanceId int) *config {
+	db, repositoryHandler, err := database.NewSQLDatabaseFactory(instanceId)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	c.db = db
+	c.repositoryHandler = repositoryHandler
+	return c
+}
+
+func (c *config) Server(instanceId int) *config {
+	server, err := api.NewServerInstanceFactory(instanceId, c.repositoryHandler)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	c.server = server
+	return c
+}
+
+func (c *config) StartServer() {
+	c.server.Listen()
+}
+
+func (c *config) ConnectDatabase() {
+	if err := c.db.Connect(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func (c *config) DisconnectDatabase() {
+	c.db.Disconnect()
 }
