576a976b0386b1100dad42c9795158f7979379f0
duardoqueiroz <eduardoldq1@gmail.com>
2024-06-29 19:51:32 +0000

    fix: remove inputs and outputs

--- pkg/domain/usecases/user.go
+++ pkg/domain/usecases/user.go
@@ -1,44 +1,14 @@
 package usecases
 
-type CreateUserInput struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	CPF      string `json:"cpf"`
-	Phone    string `json:"phone"`
-	Password string `json:"password"`
-}
+import (
+	"github.com/duardoqueiroz/my-finances-golang/pkg/application/inputs"
+	"github.com/duardoqueiroz/my-finances-golang/pkg/application/outputs"
+)
 
-type CreateUserOutput struct {
-	ID string
-}
-
-type UpdateUserInput struct {
-	Name  string
-	Email string
-	CPF   string
-	Phone string
-}
-
-type FindAllUserOutput struct {
-	ID    string
-	Name  string
-	Email string
-	CPF   string
-	Phone string
-}
-
-type FindUserByIDOutput struct {
-	ID    string
-	Name  string
-	Email string
-	CPF   string
-	Phone string
-}
-
-type UserUseCase interface {
-	Create(input CreateUserInput) (*CreateUserOutput, error)
-	Update(id string, input UpdateUserInput) error
-	FindAll() ([]FindAllUserOutput, error)
-	FindByID(id string) (*FindUserByIDOutput, error)
+type User interface {
+	Create(input inputs.CreateUserInput) (*outputs.CreateUserOutput, error)
+	Update(id string, input inputs.UpdateUserInput) error
+	FindAll() ([]outputs.FindAllUserOutput, error)
+	FindByID(id string) (*outputs.FindUserByIDOutput, error)
 	Delete(id string) error
 }
