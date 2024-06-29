fc6258f62a441d81a62969556efcb3dc601fd125
duardoqueiroz <eduardoldq1@gmail.com>
2024-06-29 19:50:22 +0000

    feat: encrypt user password with bcrypt

--- pkg/domain/entities/user.go
+++ pkg/domain/entities/user.go
@@ -1,49 +1,52 @@
 package entities
 
 import (
-	"fmt"
-
 	"github.com/duardoqueiroz/my-finances-golang/pkg/domain/keyvalues"
 	"github.com/google/uuid"
 )
 
 type User struct {
-	id    string
-	name  *keyvalues.Name
-	email *keyvalues.Email
-	cpf   *keyvalues.CPF
-	phone *keyvalues.Phone
+	id       string
+	name     *keyvalues.Name
+	email    *keyvalues.Email
+	cpf      *keyvalues.CPF
+	phone    *keyvalues.Phone
+	password *keyvalues.Password
 }
 
-func NewUser(name, email, cpf, phone string) (*User, error) {
+func NewUser(name, email, cpf, phone, password string) (*User, error) {
 	nameValue, err := keyvalues.NewName(name)
 	if err != nil {
-		return nil, fmt.Errorf("error creating user: %w", err)
+		return nil, err
 	}
 
 	emailValue, err := keyvalues.NewEmail(email)
 	if err != nil {
-		return nil, fmt.Errorf("error creating user: %w", err)
+		return nil, err
 	}
 
 	cpfValue, err := keyvalues.NewCPF(cpf)
 	if err != nil {
-		return nil, fmt.Errorf("error creating user: %w", err)
+		return nil, err
 	}
 
 	phoneValue, err := keyvalues.NewPhone(phone)
 	if err != nil {
-		return nil, fmt.Errorf("error creating user: %w", err)
+		return nil, err
 	}
 
-	id := uuid.New().String()
+	passwordValue, err := keyvalues.NewPassword(password)
+	if err != nil {
+		return nil, err
+	}
 
 	return &User{
-		id:    id,
-		name:  nameValue,
-		email: emailValue,
-		cpf:   cpfValue,
-		phone: phoneValue,
+		id:       uuid.New().String(),
+		name:     nameValue,
+		email:    emailValue,
+		cpf:      cpfValue,
+		phone:    phoneValue,
+		password: passwordValue,
 	}, nil
 }
 
@@ -53,4 +56,20 @@ func (u User) ID() string {
 
 func (u User) Name() string {
 	return u.name.Value()
+}
+
+func (u User) Email() string {
+	return u.email.Value()
+}
+
+func (u User) CPF() string {
+	return u.cpf.Value()
+}
+
+func (u User) Phone() string {
+	return u.phone.Value()
+}
+
+func (u User) Password() string {
+	return u.password.Value()
 }
---
+++ pkg/domain/keyvalues/password.go
@@ -0,0 +1,60 @@
+package keyvalues
+
+import (
+	"fmt"
+	"regexp"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Password struct {
+	value string
+}
+
+func NewPassword(value string) (*Password, error) {
+	if !isPasswordSecure(value) {
+		return nil, fmt.Errorf("password must have at least 8 characters, one lowercase letter, one uppercase letter, one number and one special character")
+	}
+
+	hash, err := hashPassword(value)
+	if err != nil {
+		return nil, fmt.Errorf("error hashing password: %w", err)
+	}
+
+	return &Password{value: hash}, nil
+}
+
+func hashPassword(pwd string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
+
+func (p Password) Value() string {
+	return p.value
+}
+
+func (p Password) Compare(pwd string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(p.value), []byte(pwd))
+	if err != nil {
+		return fmt.Errorf("invalid password")
+	}
+
+	return nil
+}
+
+func isPasswordSecure(pwd string) bool {
+	secure := true
+	tests := []string{".{8,}", "[a-z]", "[A-Z]", "[0-9]", "[^\\d\\w]"}
+	for _, test := range tests {
+		t, _ := regexp.MatchString(test, pwd)
+		if !t {
+			secure = false
+			break
+		}
+	}
+	return secure
+}
