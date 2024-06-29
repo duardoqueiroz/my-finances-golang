9592815b6c950b2985100b61ec3f1ea00616f77d
duardoqueiroz <eduardoldq1@gmail.com>
2024-06-29 19:47:44 +0000

    feat: init app with new configuration strategy

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,35 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/duardoqueiroz/my-finances-golang/pkg/config"
-	"github.com/duardoqueiroz/my-finances-golang/pkg/factories"
-	"github.com/duardoqueiroz/my-finances-golang/pkg/infra/api/routes"
-	"github.com/duardoqueiroz/my-finances-golang/pkg/infra/database/repositories/postgres"
+	"github.com/duardoqueiroz/my-finances-golang/pkg/infra/api"
+	"github.com/duardoqueiroz/my-finances-golang/pkg/infra/database"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	conf, err := config.ReadConfig()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	godotenv.Load()
 
-	db := postgres.SetupCredentials(conf.Database.User, conf.Database.Password, conf.Database.Port, conf.Database.Name, conf.Database.Host)
-	if err := db.Connect(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer db.Disconnect()
+	app := config.New().AppName("My finances")
+	app.Database(database.PostgresInstance).ConnectDatabase()
+	defer app.DisconnectDatabase()
 
-	conn := db.Connection()
-
-	repoFactory := factories.NewRepositoryFactory(postgres.NewUserRepository(conn))
-
-	server := routes.LoadRoutes(repoFactory)
-	server.Debug = true
-	address := fmt.Sprintf("%s:%s", conf.Server.Host, conf.Server.Port)
-	server.Logger.Info(server.Start(address))
+	app.Server(api.EchoInstance).StartServer()
 }
