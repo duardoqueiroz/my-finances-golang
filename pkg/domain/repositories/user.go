b4035c9b67241bf470f3d5a8d189e28fa41bfa4b
duardoqueiroz <eduardoldq1@gmail.com>
2024-06-29 19:51:16 +0000

    feat: return id after user creation

--- pkg/domain/repositories/user.go
+++ pkg/domain/repositories/user.go
@@ -7,7 +7,7 @@ import (
 type UserRepository interface {
 	FindAll() ([]entities.User, error)
 	FindByID(id string) (*entities.User, error)
-	Create(user *entities.User) error
+	Create(user *entities.User) (string, error)
 	Update(id string, user *entities.User) error
 	Delete(id string) error
 }
