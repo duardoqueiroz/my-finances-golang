8f884a71236e12028f99e2781120c2967625401a
duardoqueiroz <eduardoldq1@gmail.com>
2024-07-01 14:32:34 +0000

    feat: instantiate logger

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,13 +4,14 @@ import (
 	"github.com/duardoqueiroz/my-finances-golang/pkg/config"
 	"github.com/duardoqueiroz/my-finances-golang/pkg/infra/api"
 	"github.com/duardoqueiroz/my-finances-golang/pkg/infra/database"
+	"github.com/duardoqueiroz/my-finances-golang/pkg/infra/logger"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
 
-	app := config.New().AppName("My finances")
+	app := config.New().AppName("My finances").Logger(logger.ZapInstance)
 	app.Database(database.PostgresInstance).ConnectDatabase()
 	defer app.DisconnectDatabase()
 
